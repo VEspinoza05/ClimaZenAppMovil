6fc5341326750ad3298696161ee57b278634c4b3
VEspinoza05 <147788658+VEspinoza05@users.noreply.github.com>
2025-06-18 10:35:32 -0600

    Agregar pantalla de leccion de curso

---
+++ leccionCurso.py
@@ -0,0 +1,20 @@
+from kivy.lang import Builder
+from kivymd.app import MDApp
+from kivy.core.window import Window
+from kivy.modules import inspector
+from kivy.uix.boxlayout import BoxLayout
+
+Window.size = (414, 896)
+
+class MyWidget(BoxLayout):
+    pass
+
+
+class leccionCurso(MDApp):
+    def build(self):    
+        root = MyWidget()
+        inspector.create_inspector(Window, root)
+        self.theme_cls.theme_style = "Light"
+        return MyWidget()
+    
+leccionCurso().run()
