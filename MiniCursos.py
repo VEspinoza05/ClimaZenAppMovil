c84aedf2856896d51e66d4ad34ff1157fa4bd6e9
VEspinoza05 <147788658+VEspinoza05@users.noreply.github.com>
2025-06-17 18:39:11 -0600

    Agregar pantalla de MiniCursos seleccionados por usuario

---
+++ MiniCursos.py
@@ -0,0 +1,7 @@
+from kivymd.uix.screen import MDScreen
+from kivy.properties import StringProperty, NumericProperty
+from kivymd.uix.card import MDCard
+from kivy.lang import Builder
+
+class MiniCursos(MDScreen):
+    pass
--- NavBar.py
+++ NavBar.py
@@ -2,10 +2,14 @@ from kivy.lang import Builder
 from kivymd.app import MDApp
 from kivy.core.window import Window
 
+from MiniCursos import MiniCursos
+
 Window.size = (414, 896)
 
 class NavBar(MDApp):
     def build(self):
+        Builder.load_file("MiniCursos.kv")
+
         self.theme_cls.theme_style = "Light"
         return Builder.load_file('NavBar.kv')
     
